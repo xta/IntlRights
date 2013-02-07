c055dc950bd081e2a1c9b0c2cb6acad6928e1b21
Rex Feng <rexfeng@gmail.com>
2013-02-07 16:19:02 -0500

    build and pass video tests

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,4 +1,6 @@
 class Country < ActiveRecord::Base
+  attr_accessible :name, :alpha2, :alpha3, :numeric
+
   has_many :policies, :dependent => :destroy
   has_many :videos, :through => :policies
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,5 +1,26 @@
 require 'spec_helper'
 
 describe Video do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before(:each) do
+    @video = FactoryGirl.create(:video)
+    6.times { @video.countries << FactoryGirl.create(:country) }
+  end
+
+  context 'for a video' do
+
+    describe '.policies' do
+      it 'lists all policies' do
+        @video.policies.size.should eql(6)
+      end
+    end
+
+    describe '.countries' do
+      it 'lists all countries' do
+        @video.countries.size.should eql(6)
+      end
+    end
+
+  end
+
 end
