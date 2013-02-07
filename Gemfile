5116f41c79f68db20be4117ead71e4d33d78554a
Rex Feng <rexfeng@gmail.com>
2013-02-07 16:12:06 -0500

    add iso code gem and seed with 6 countries

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 gem 'rails', '3.2.11'
 gem 'jquery-rails'
 gem 'pg'
+gem 'iso_country_codes'
 
 group :assets do
   gem 'uglifier', '>= 1.0.3'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Countries
+  # United States, Canada, China, Mexico, India, and Taiwan
+
+  countries = %w(us ca ch mx in tw)
+
+  countries.each do |country|
+    code = IsoCountryCodes.find(country)
+    Country.create!(:name => code.name, :alpha2 => code.alpha2, :alpha3 => code.alpha3, :numeric => code.numeric)
+  end
+
+# Videos
