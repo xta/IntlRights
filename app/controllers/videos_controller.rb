367ed314d7491fd6bf85e430fd6d948e94143f46
Rex Feng <rexfeng@gmail.com>
2013-02-08 22:41:55 -0500

    refactor includes policies into model

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,17 +1,17 @@
 class VideosController < ApplicationController
 
   def index
-    @videos = Video.includes(:countries, :policies).page(params[:page])
+    @videos = Video.includes(:countries).page(params[:page])
   end
 
   def show
     @country = Country.by_iso( params[:iso_code] )
-    @video = Video.includes(:policies).by_id_and_country(params[:id], @country) if @country
+    @video = Video.by_id_and_country(params[:id], @country) if @country
   end
 
   def country
     @country = Country.by_iso( params[:iso_code] )
-    @videos = Video.includes(:policies).by_country(@country).page(params[:page]) if @country
+    @videos = Video.by_country(@country).page(params[:page]) if @country
   end
 
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,19 +1,25 @@
 class Video < ActiveRecord::Base
-  attr_accessible :vid
+  # relations
+    attr_accessible :vid
 
-  validates :vid, :uniqueness => true
+    has_many :policies, :dependent => :destroy
+    has_many :countries, :through => :policies
 
-  has_many :policies, :dependent => :destroy
-  has_many :countries, :through => :policies
+  #validation
+    validates :vid, :uniqueness => true
 
-  paginates_per 100
+  #pagination
+    paginates_per 100
 
-  def self.by_id_and_country(id, country)
-    find(id, :conditions => { policies: {country_id: country.id} })
-  end
+  # scopes
+    default_scope { includes(:policies) }
 
-  def self.by_country(country)
-    where(policies: {country_id: country.id})
-  end
+    def self.by_id_and_country(id, country)
+      find(id, :conditions => { policies: {country_id: country.id} })
+    end
+
+    def self.by_country(country)
+      where(policies: {country_id: country.id})
+    end
 
 end
