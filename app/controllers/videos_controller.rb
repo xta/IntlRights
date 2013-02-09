a4eb8a7592cb2be77097709be95f42a984004df3
Rex Feng <rexfeng@gmail.com>
2013-02-08 23:35:09 -0500

    add json response

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,17 +1,32 @@
 class VideosController < ApplicationController
 
   def index
-    @videos = Video.includes(:countries).page(params[:page])
+    @videos = Video.includes(:countries)
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @videos }
+    end
   end
 
   def show
     @country = Country.by_iso( params[:iso_code] )
     @video = Video.by_id_and_country(params[:id], @country) if @country
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @video }
+    end
   end
 
   def country
     @country = Country.by_iso( params[:iso_code] )
     @videos = Video.by_country(@country).page(params[:page]) if @country
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @videos }
+    end
   end
 
 end
--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -2,28 +2,66 @@ require 'spec_helper'
 
 describe VideosController do
 
-  describe '.index' do
-    before :each do 
-      get :index
+  context 'for HTML' do
+
+    describe '.index' do
+      before :each do 
+        get :index
+      end
+
+      it { should render_template("index") }
     end
 
-    it { should render_template("index") }
+    describe '.country' do
+      before :each do 
+        get :country, :iso_code => 'US'
+      end
+
+      it { should render_template("country") }
+    end
+
+    describe '.show' do
+      before :each do 
+        get :show, :id => 1, :iso_code => 'US'
+      end
+
+      it { should render_template("show") }
+    end
+
   end
 
-  describe '.country' do
-    before :each do 
-      get :country, :iso_code => 'US'
+  context 'for JSON' do
+
+    describe '.index' do
+      before :each do 
+        get 'index', :format => :json
+      end
+
+      it "should be successful" do
+        response.should be_success
+      end
     end
 
-    it { should render_template("country") }
-  end
+    describe '.country' do
+      before :each do 
+        get :country, :iso_code => 'US', :format => :json
+      end
 
-  describe '.show' do
-    before :each do 
-      get :show, :id => 1, :iso_code => 'US'
+      it "should be successful" do
+        response.should be_success
+      end
     end
 
-    it { should render_template("show") }
+    describe '.show' do
+      before :each do
+        get :show, :id => 1, :iso_code => 'US', :format => :json
+      end
+
+      it "should be successful" do
+        response.should be_success
+      end
+    end
+
   end
 
 end
