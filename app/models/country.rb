ad1e0b2af3459af6af5e67b51e5f99cd61bcafe6
Rex Feng <rexfeng@gmail.com>
2013-02-11 00:21:40 -0500

    add comment to group model logic

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,13 +1,15 @@
 class Country < ActiveRecord::Base
-  attr_accessible :name, :alpha2, :alpha3, :numeric
+  # relations
+    attr_accessible :name, :alpha2, :alpha3, :numeric
+    
+    has_many :policies, :dependent => :destroy
+    has_many :videos, :through => :policies
 
-  validates :name, :uniqueness => true
-  validates :alpha2, :uniqueness => true
-  validates :alpha3, :uniqueness => true
-  validates :numeric, :uniqueness => true
-
-  has_many :policies, :dependent => :destroy
-  has_many :videos, :through => :policies
+  #validation
+    validates :name, :uniqueness => true
+    validates :alpha2, :uniqueness => true
+    validates :alpha3, :uniqueness => true
+    validates :numeric, :uniqueness => true
 
   def self.by_iso(alpha2_param)
     self.find_by_alpha2(alpha2_param.upcase)
--- app/models/policy.rb
+++ app/models/policy.rb
@@ -1,14 +1,15 @@
 class Policy < ActiveRecord::Base
+  # relations
+    attr_accessible :video_id, :country_id, :rights
 
-  attr_accessible :video_id, :country_id, :rights
+    belongs_to :video
+    belongs_to :country
 
   #constant
-  POLICY_RIGHTS    = %w( Free Monetize Block )
-  AVAILABLE_RIGHTS = %w( Free Monetize )
+    POLICY_RIGHTS    = %w( Free Monetize Block )
+    AVAILABLE_RIGHTS = %w( Free Monetize )
 
-  validates_inclusion_of :rights, :in => POLICY_RIGHTS
-
-  belongs_to :video
-  belongs_to :country
+  #validation
+    validates_inclusion_of :rights, :in => POLICY_RIGHTS
 
 end
